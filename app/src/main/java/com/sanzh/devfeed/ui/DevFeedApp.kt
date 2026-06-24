203d75a67b80edb1df8e2c2785eac5405d69b13f
Sanzhar <omarkhanovsanzh@gmail.com>
2026-06-25 01:59:42 +0500

    Deleted blocker

--- app/src/main/java/com/sanzh/devfeed/ui/DevFeedApp.kt
+++
@@ -1,9 +0,0 @@
-package com.sanzh.devfeed.ui
-
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-
-@Composable
-fun DevFeedApp() {
-    Text(text = "Welcome to DevFeed")
-}
